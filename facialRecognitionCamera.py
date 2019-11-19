a79137253b547588e4303b11c85787a3cf3ad4c1
AydBlot <jobl9833@colorado.edu>
2019-11-19 12:22:07 -0700

    Connection MQTT client and communication with lambda function now functional.

--- facialRecognitionCamera.py
+++ facialRecognitionCamera.py
@@ -10,16 +10,46 @@ import os
 import threading
 from threading import Lock 
 from videoCamera import VideoCamera
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+#Intialize a client for MQTT
+myMQTTClient = AWSIoTMQTTClient("myClientID")
 
 #Create video Camera object
 video_camera = VideoCamera(flip=False) # creates a camera object, flip vertically
 
 #teach openCV how to recognize a face with an XML file
 object_classifier = cv2.CascadeClassifier("/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml")
-image_delay_time = 0
+
 #Initialize the boto3 client to conenct to aws
 S3 = boto3.client('s3')
 
+print("Connecting to AWS MQTT Broker...")
+# AWS IoT certificate based connection
+#myMQTTClient.configureEndpoint("a3aj4aophv141l-ats.iot.us-west-2.amazonaws.com", 8883)
+myMQTTClient.configureEndpoint("a3aj4aophv141l-ats.iot.us-east-1.amazonaws.com", 8883)
+myMQTTClient.configureCredentials("/home/pi/EID/finalProject/certs/Amazon_Root_CA_1.pem", "/home/pi/EID/finalProject/certs/7583bb3ead-private.pem.key", "/home/pi/EID/finalProject/certs/7583bb3ead-certificate.pem.crt")
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(100)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(20)  # 5 sec
+myMQTTClient.configureAutoReconnectBackoffTime(1, 128, 20)
+myMQTTClient.configureOfflinePublishQueueing(-1)
+
+#connect and publish
+myMQTTClient.connect()
+print("Successfully Connected to Broker")
+
+def confirm_receive(self, params, packet):
+    print("--------new Payload------")
+    print("Topic: {} ".format(packet.topic))
+    print("Message: {} ".format(packet.payload))
+
+#Subscribe the MQTT client to a topic
+myMQTTClient.subscribe("facial/recognition/confirm", 1, confirm_receive)
+
+
+#Creates a temporary .jpg file in a specified basePath
 class TempImage:
     def __init__(self, basePath="/home/pi/images", ext=".jpg"):
 
@@ -36,7 +66,7 @@ SOURCE_FILENAME = '/home/pi/images/test.jpg'
 NEW_FACE_BUCKET_NAME = 'facialrecnewfacebucket'
 
 def check_for_face():
-    global image_delay_time
+    image_delay_time = 0
     while True:
         try:
             _, found_face, frame = video_camera.get_object(object_classifier)
@@ -49,6 +79,8 @@ def check_for_face():
                 print(t.path)
                 cv2.imwrite(t.path, frame)
                 S3.upload_file(t.path, NEW_FACE_BUCKET_NAME, t.path)
+                #Update the read permissions so the lambda function can access the newly added s3 object
+                os.system("aws s3api put-object-acl --bucket {0} --key {1} --acl public-read".format(NEW_FACE_BUCKET_NAME, t.path))
                 image_delay_time = time.time()
         except:
             print("camera object not working:", sys.exc_info()[0])
