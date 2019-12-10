dc279e5857ac2b8cb65d0334e77244f4219991dd
AydBlot <jobl9833@colorado.edu>
2019-12-10 12:33:11 -0700

    Integrated microphone to allow for password input and new users to be added to known faces S3

--- facialRecognitionCamera.py
+++ facialRecognitionCamera.py
@@ -1,3 +1,5 @@
+#Referenced just4give/home-surveillance github repo
+import speech_recognition as sr  
 import cv2
 import boto3
 import sys
@@ -23,6 +25,7 @@ object_classifier = cv2.CascadeClassifier("/home/pi/opencv/data/haarcascades/haa
 
 #Initialize the boto3 client to conenct to aws
 S3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 print("Connecting to AWS MQTT Broker...")
 # AWS IoT certificate based connection
@@ -40,16 +43,55 @@ myMQTTClient.configureOfflinePublishQueueing(-1)
 myMQTTClient.connect()
 print("Successfully Connected to Broker")
 
+NEW_FACE_BUCKET_NAME = 'facialrecnewfacebucket'
+KNOWN_FACE_BUCKET_NAME ='facialrecknownfacebucket' 
+
+#callback function that is triggered whenever a new MQTT message is published
 def confirm_receive(self, params, packet):
     print("--------new Payload------")
     print("Topic: {} ".format(packet.topic))
     print("Message:{}".format(packet.payload))
+    split_payload = str(packet.payload).split('image ')
+    print("split: {}".format(split_payload))
+    name = split_payload[1].split('.')
+    print(name[0])
+    keyname = split_payload[1].split(' ')
+    print('name: {}'.format(keyname[0]))
     if "Matches" in str(packet.payload):
-        split_payload = str(packet.payload).split('Matches ')
-        name = split_payload[1].split('.')
         os.system("espeak 'Welcome back, {}!' --stdout | aplay".format(name[0]))
+        return
     else:
-        os.system("espeak 'You are not welcome here.' --stdout | aplay")
+        os.system("espeak 'You are not recognized in the system, please say the password to be entered.' --stdout | aplay")
+        while(1):
+            print("test")
+            r = sr.Recognizer()                                                                                   
+            str1 = "don't panic"
+            with sr.Microphone() as source:                                                                       
+                print("Speak:")                                                                                   
+                audio = r.listen(source)   
+
+            try:
+                str2 =  r.recognize_google(audio)
+                print("You said " + str2)
+                if str2 == str1:
+                    print("strings match")
+                    os.system("espeak 'Welcome to the system' --stdout | aplay")
+                    source = {
+                            'Bucket': NEW_FACE_BUCKET_NAME,
+                            'Key': keyname[0]
+                            }
+                    destinationKeyname=keyname[0].split('/')
+                    print(destinationKeyname[4])
+                    s3.meta.client.copy(source, 'facialrecknownfacebucket',  destinationKeyname[4]) 
+                    os.system("aws s3api put-object-acl --bucket {0} --key {1} --acl public-read".format(KNOWN_FACE_BUCKET_NAME, destinationKeyname[4]))
+                    break
+                else:
+                    print("Access Denied")
+                    break
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results; {0}".format(e))
 
 #Subscribe the MQTT client to a topic
 myMQTTClient.subscribe("facial/recognition/confirm", 1, confirm_receive)
@@ -68,7 +110,6 @@ class TempImage:
 		# remove the file
         os.remove(self.path)
 
-NEW_FACE_BUCKET_NAME = 'facialrecnewfacebucket'
 
 #Function that checks for a face in the videstream every 5 seconds
 def check_for_face():
